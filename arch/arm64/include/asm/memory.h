8b45efbf6dab85ccd1777621f605b13da0adfc1e
qmyyxtl <qmyyxtl@sjtu.edu.cn>
2026-07-09 21:58:21 +0800

    KVM: arm64: allow Normal-NC stage-2 mappings with FWB

--- arch/arm64/include/asm/memory.h
+++ arch/arm64/include/asm/memory.h
@@ -152,9 +152,10 @@
 
 /*
  * Memory types for Stage-2 translation when ID_AA64MMFR2_EL1.FWB is 0001
- * Stage-2 enforces Normal-WB and Device-nGnRE
+ * Stage-2 encodes Normal-WB, Normal-NC and Device-nGnRE explicitly.
  */
 #define MT_S2_FWB_NORMAL	6
+#define MT_S2_FWB_NORMAL_NC	5
 #define MT_S2_FWB_DEVICE_nGnRE	1
 
 #ifdef CONFIG_ARM64_4K_PAGES
--- arch/arm64/kvm/hyp/pgtable.c
+++ arch/arm64/kvm/hyp/pgtable.c
@@ -703,13 +703,10 @@ static int stage2_set_prot_attr(struct kvm_pgtable *pgt, enum kvm_pgtable_prot p
 
 	if (device && normal_nc)
 		return -EINVAL;
-	if (normal_nc && stage2_has_fwb(pgt))
-		return -EINVAL;
-
 	if (device)
 		attr = KVM_S2_MEMATTR(pgt, DEVICE_nGnRE);
 	else if (normal_nc)
-		attr = PTE_S2_MEMATTR(MT_S2_NORMAL_NC);
+		attr = KVM_S2_MEMATTR(pgt, NORMAL_NC);
 	else
 		attr = KVM_S2_MEMATTR(pgt, NORMAL);
 	u32 sh = KVM_PTE_LEAF_ATTR_LO_S2_SH_IS;
